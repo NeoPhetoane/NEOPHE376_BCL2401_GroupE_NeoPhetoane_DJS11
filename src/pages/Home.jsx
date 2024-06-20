89799a12bc4f4c5cbf8944631bbd6cc45fafdc00
NeoPhetoane <phetoaneneo4@gmail.com>
2024-06-20 22:15:14 +0200

    Home component content added to fetch from an API and show previews of the shows.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-export default function Home() {
+
+function TruncateText({ text, maxLength }) {
+    const truncatedText = text.length > maxLength ? text.slice(0, maxLength) + "..." : text;
+    return <span className="truncate">{truncatedText}</span>;
+  }
+
+function Home() {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(null);
+
+
+  useEffect(() => {
+    const fetchPodcastData = async () => {
+      try {
+        const response = await fetch("https://podcast-api.netlify.app/");
+        if (!response.ok) {
+          throw new Error("Data fetching failed.");
+        }
+        const data = await response.json();
+
+        // Sort the posts alphabetically by title
+        const sortedData = data.sort((a, b) => a.title.localeCompare(b.title));
+
+
+        setPosts(sortedData); // Updates state with fetched posts
+      } catch (error) {
+        setError(error.message); //Updates state with error message
+      }
+    };
+
+    fetchPodcastData();
+  }, []); //  Empty dependency array
+
+
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <h1>Podcasts</h1>
+      {/* Ternary operator that manages what displays on the page */}
+
+      {error ? (
+        <div className="error-message">
+          <p>Error: {error}</p>
+        </div>
+      ) : (
+        <ol>
+          {/* Itterates over the API data and looks within to find specified data and displays it */}
+          {posts.map((post) => (
+                <li key={post.id} className="card" >
+                    <Link to={`/seasons/${post.id}`} className="card-link">
+                        <h2>{post.title}</h2>
+                        <img src={post.image} />
+                        <TruncateText text={post.description} maxLength={150}/>
+                        <p>Seasons: {post.seasons}</p>
+                        <p>Genre: {post.genres}</p>
+                        <p>Last Updated: {post.updated}</p>
+                    </Link>
+                </li>
+          ))}
+        </ol>
+        )}
+        </>
+      );
+    }
+    
+    export default Home;
