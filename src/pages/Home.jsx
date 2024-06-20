988a745c653493545ff890edd24aa265de879706
NeoPhetoane <phetoaneneo4@gmail.com>
2024-06-21 00:04:19 +0200

    Loading state added to Home.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,32 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-
+//Function to cut the discription length
 function TruncateText({ text, maxLength }) {
     const truncatedText = text.length > maxLength ? text.slice(0, maxLength) + "..." : text;
     return <span className="truncate">{truncatedText}</span>;
   }
 
+  //Function to format date
+
+  function formatUpdatedDate(isoDateString) {
+    const date = new Date(isoDateString);
+    return date.toLocaleDateString('en-US', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    });
+  }
+
 function Home() {
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
 
   useEffect(() => {
     const fetchPodcastData = async () => {
+      setLoading(true);
       try {
         const response = await fetch("https://podcast-api.netlify.app/");
         if (!response.ok) {
@@ -28,8 +41,10 @@ function Home() {
         setPosts(sortedData); // Updates state with fetched posts
       } catch (error) {
         setError(error.message); //Updates state with error message
-      }
+      } finally {
+        setLoading(false); // Set loading to false regardless of success or error
     };
+  }
 
     fetchPodcastData();
   }, []); //  Empty dependency array
@@ -41,7 +56,10 @@ function Home() {
       <h1>Podcasts</h1>
       {/* Ternary operator that manages what displays on the page */}
 
-      {error ? (
+
+        {loading ? (
+          <h2>Loading...</h2>
+        ) : error ? (
         <div className="error-message">
           <p>Error: {error}</p>
         </div>
@@ -56,7 +74,7 @@ function Home() {
                         <TruncateText text={post.description} maxLength={150}/>
                         <p>Seasons: {post.seasons}</p>
                         <p>Genre: {post.genres}</p>
-                        <p>Last Updated: {post.updated}</p>
+                        <p>Last Updated: {formatUpdatedDate (post.updated)}</p>
                     </Link>
                 </li>
           ))}
